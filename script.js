0b21167818a6c11fa3a70708cf26cbecc2ba22e4
Ruiyot Abby <ruiyotabby@gmail.com>
2023-03-27 00:47:13 +0300

    Adds an event listener which prevents a user from registering with passwords which do not match

--- script.js
+++ script.js
@@ -1,12 +1,19 @@
 const password1 = document.getElementById('password1')
 const password2 = document.getElementById('password2')
+const form = document.querySelector('form')
 
 window.addEventListener('load',check_password)
 password1.addEventListener('input', e => check_password(e))
 password2.addEventListener('input', e => check_password(e))
 
+form.addEventListener('submit', (e) => {
+  if ((password1.value != password2.value) || ((password1.value && password2.value) == '')) {
+    e.preventDefault();
+  }
+})
+
 function check_password() {
-  if (password1.value != password2.value) {
+  if ((password1.value != password2.value) || ((password1.value && password2.value) == '')) {
     password1.classList.add('error')
     password2.classList.add('error')
   }
